862a8f99adb38435a7316259c650c98b368a9ac5
Nevin Chen <38507731+Nevin-Chen@users.noreply.github.com>
2025-10-22 21:59:02 -0400

    Use FMP API in favor of Polygon (#4)
    * Use FMP API in favor of Polygon
    
    * Fix interface for OHLC format
    
    * Use one year of historical data

---
+++ src/fmp/fmp.ts
@@ -0,0 +1,35 @@
+export interface OHLC {
+  symbol: string;
+  date: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  change: number;
+  changePercent: number;
+  vwap: number;
+}
+
+export interface OHLCResponse {
+  data: OHLC[];
+}
+
+export interface CompanyProfile {
+  symbol: string;
+  marketCap: number;
+  companyName: string;
+  description: string;
+  exchange: string;
+  sector: string;
+  ceo: string;
+  employees: number;
+  industry: string;
+  website: string;
+  address: string;
+  country: string;
+  city: string;
+  state: string;
+  zip: string
+  ipoDate: string;
+}
