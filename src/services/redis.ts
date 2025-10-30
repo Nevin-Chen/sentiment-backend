df45740d29b1a2e35f8d22bffc48e3ee175faa0c
Nevin Chen <38507731+Nevin-Chen@users.noreply.github.com>
2025-10-29 20:42:01 -0400

    Fix profile caching (#9)

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -21,7 +21,6 @@ async function getCache<T>(key: string): Promise<T | undefined> {
     if (!data) return;
 
     const parsedData = JSON.parse(data);
-    console.log(`REDIS Retrieved cache key: ${key}`);
     return parsedData as T;
   } catch (err) {
     console.error(`REDIS Failed to get cache key: ${key} - `, err);
@@ -32,7 +31,6 @@ async function getCache<T>(key: string): Promise<T | undefined> {
 async function setCache(key: string, value: any, ttlSeconds: number): Promise<void> {
   try {
     await redisClient.set(key, JSON.stringify(value), { EX: ttlSeconds });
-    console.log(`REDIS Set cache key: ${key} with TTL: ${ttlSeconds}s`);
   } catch (err) {
     console.error(`REDIS Failed to set key: ${key} - `, err);
   }
