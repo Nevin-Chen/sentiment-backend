1c6d40b5486f6211372d527a87317d06af7a7d02
nevin-chen <nevinchen6@gmail.com>
2025-10-31 13:18:11 -0400

    Refactor FMP ohlc and profile service typing

--- src/controllers/fmpController.ts
+++ src/controllers/fmpController.ts
@@ -8,9 +8,7 @@ export class FMPController extends Controller {
   private service = new FMPService();
 
   @Get('{symbol}/profile')
-  public async getProfile(
-    @Path() symbol: string
-  ): Promise<CompanyProfile> {
+  public async getProfile(@Path() symbol: string): Promise<CompanyProfile> {
     return this.service.getProfile(symbol);
   }
 }
--- src/types/fmp.ts
+++ src/types/fmp.ts
@@ -1,3 +1,58 @@
+export type FMPOhlcResponse = FMPOhlc[]
+export type FMPProfileResponse = FMPProfile[]
+
+export interface FMPProfile {
+  symbol: string;
+  price: number;
+  marketCap: number;
+  beta: number;
+  lastDividend: number;
+  range: string;
+  change: number;
+  changePercentage: number;
+  volume: number;
+  averageVolume: number;
+  companyName: string;
+  currency: string;
+  cik: string;
+  isin: string;
+  cusip: string;
+  exchangeFullName: string;
+  exchange: string;
+  industry: string;
+  website: string;
+  description: string;
+  ceo: string;
+  sector: string;
+  country: string;
+  fullTimeEmployees: string;
+  phone: string;
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+  image: string;
+  ipoDate: string;
+  defaultImage: boolean;
+  isEtf: boolean;
+  isActivelyTrading: boolean;
+  isAdr: boolean;
+  isFund: boolean;
+}
+
+export interface FMPOhlc {
+  symbol: string;
+  date: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  change: number;
+  changePercent: number;
+  vwap: number;
+}
+
 export interface CompanyProfile {
   address: string;
   ceo: string;
@@ -5,7 +60,7 @@ export interface CompanyProfile {
   companyName: string;
   country: string;
   description: string;
-  employees: number;
+  employees: string;
   industry: string;
   ipoDate: string;
   marketCap: number;
